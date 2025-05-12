54ae1f4cc03419cbfbe2daf1daf3062106d38930
Mohammed Draimli <m_fares@live.com>
2025-05-12 19:30:04 +0300

    Allow Social node to be first element

--- src/components/WorkflowEditor.tsx
+++ src/components/WorkflowEditor.tsx
@@ -571,18 +571,18 @@ const WorkflowEditorContent: React.FC = () => {
       requestedChildType = childTypeOrNext;
     }
     // RULE 1: Block Social Post on TopicalKeyword
-    if (parentNode?.type === 'topicalKeyword' && requestedChildType === 'socialMedia') {
-      alert('You cannot add a Social Post node to a TopicalKeyword node.');
-      return;
-    }
+    // if (parentNode?.type === 'topicalKeyword' && requestedChildType === 'socialMedia') {
+    //   alert('You cannot add a Social Post node to a TopicalKeyword node.');
+    //   return;
+    // }
     // RULE 2: Block Social Post on any node with outgoing edge
-    if (requestedChildType === 'socialMedia') {
-      const hasOutgoing = edges.some(e => e.source === parentId);
-      if (hasOutgoing) {
-        alert('You cannot add a Social Post node to a node that is already connected to another node on the right.');
-        return;
-      }
-    }
+    // if (requestedChildType === 'socialMedia') {
+    //   const hasOutgoing = edges.some(e => e.source === parentId);
+    //   if (hasOutgoing) {
+    //     alert('You cannot add a Social Post node to a node that is already connected to another node on the right.');
+    //     return;
+    //   }
+    // }
 
     // (Moved type checks above for rule enforcement)
     if (!parentNode || parentNode.type === 'note') return;
